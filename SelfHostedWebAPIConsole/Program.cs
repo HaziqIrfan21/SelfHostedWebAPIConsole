82c9b3fb593e335575eedfaba92e06a26cb240b3
HaziqIrfan21 <90903576+HaziqIrfan21@users.noreply.github.com>
2021-11-10 19:53:01 +0800

    Add project files.

---
+++ SelfHostedWebAPIConsole/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+
+namespace SelfHostedWebAPIConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost1234");
+            var server = new HttpSelfHostServer(config, new WebApiMessageHandler());
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Self Host CMD application started");
+            Console.ReadLine();
+        }
+
+        //public void Input()
+        //{
+        //    Console.WriteLine("Please select your choice");
+        //    Console.WriteLine("1. Upper case, lower case and camelcasing");
+        //    Console.WriteLine("2. Take a string as input and return a list of string based on the input spliting character");
+        //    Console.WriteLine("3. Take a string as input and reverse it");
+        //   int res =  int.Parse(Console.ReadLine());
+
+        //    switch (res)
+        //    {
+        //        case 1:
+
+        //            Casing();
+        //            break;
+
+        //        case 2:
+                   
+
+        //            break;
+
+        //        case 3:
+                 
+
+        //            break;
+
+        //        default:
+        //            break;
+        //    }
+
+        //}
+
+        //public void Casing()
+        //{
+        //    Console.WriteLine("Input string");
+        //   string input = Console.ReadLine();
+        //    Console.WriteLine(input.ToUpper());
+        //    Console.WriteLine(input.ToLower());
+        
+
+
+
+        //}
+
+        //public void Split()
+        //{
+        //    Console.WriteLine("Input string");
+        //}
+
+        //public void Reverse()
+        //{
+        //    Console.WriteLine("Input string");
+        //}
+
+    }
+}
---
+++ SelfHostedWebAPIConsole/WebApiMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SelfHostedWebAPIConsole
+{
+    class WebApiMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var task = new Task<HttpResponseMessage>(() =>
+            {
+                HttpResponseMessage message = new HttpResponseMessage();
+                message.Content = new StringContent("I am a respone");
+                return message;
+            });
+            task.Start();
+            return task;
+        }
+    }
+}
